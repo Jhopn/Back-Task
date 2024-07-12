fff7a1d72a8449bcea28dac87d81e689953244f8
Jhoão <jhpdrnsantos2@gmail.com>
2024-07-12 13:43:11 -0300

    fix: Cors adicionado

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,12 @@ import express  from "express";
 import { RouterTask } from "./routes/TaskRouter";
 import { RouterUser } from "./routes/UserRouter";
 import { RouterAccess } from "./routes/AccessRouter";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 
 app.get("/", (req, res) => {
   res.send("Tasks Online!");
