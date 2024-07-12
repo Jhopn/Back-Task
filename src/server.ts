5bca07cda86e8bec862a60a8dec3c8194e2257b6
LPeter-nm <pg.nasmatos@outlook.com>
2024-07-12 13:49:42 -0300

    feat: importação do cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express  from "express";
+import cors from 'cors'
 import { RouterTask } from "./routes/TaskRouter";
 import { RouterUser } from "./routes/UserRouter";
 import { RouterAccess } from "./routes/AccessRouter";
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("E-Commerce Online!");
