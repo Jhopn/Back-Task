92405f2119ecf0f17f7e52b3bdbf1cc76a94b8ff
Jhoão <jhpdrnsantos2@gmail.com>
2024-07-12 22:24:36 -0300

    fix erro cors

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,11 @@ import express  from "express";
 import { RouterTask } from "./routes/TaskRouter";
 import { RouterUser } from "./routes/UserRouter";
 import { RouterAccess } from "./routes/AccessRouter";
-
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 
 app.get("/", (req, res) => {
