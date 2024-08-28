4821370e5c63f310c071ae235357c435d6ccfc8c
LPeter-nm <pg.nasmatos@outlook.com>
2024-08-28 20:46:23 -0300

    style: mudança de nome da rota

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -14,7 +14,7 @@ import {
 const router = Router();
 
 router.post("/login", loginUser)
-router.post("/register", createUser);
+router.post("/usuario/create", createUser);
 
 router.get("/usuarios", authAccess(['Admin']), stripeUser)
 router.get("/usuario/:id", authAccess(['Admin', 'Usuario']),readUser);
